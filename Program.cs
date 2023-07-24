1066bc3688ae48a58f973ca73526bcd90e901f89
Alena <weaterwind@gmail.com>
2023-07-24 17:48:36 +0700

    Add Methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿Console.WriteLine("Введите массив через пробел: ");
-string[] array = Console.ReadLine().Split(" ").ToArray();
+﻿void PrintArrayInt(string[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+string[] InputArray()
+{
+    Console.WriteLine("Введите массив через пробел: ");
+    string[] array = Console.ReadLine().Split(" ").ToArray();
+    return array;
+}
+
+string[] array = InputArray();
+
+PrintArrayInt(newArray);
